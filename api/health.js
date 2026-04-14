ba976b8f784d523b9fae1d63964581e25f59f187
viradacafe <viradanocafe@gmail.com>
2026-04-14 12:14:08 +0000

    update

--- api/health.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const key = process.env.ANTHROPIC_API_KEY;
-  res.json({
-    key_existe: !!key,
-    key_prefixo: key ? key.substring(0, 10) + '...' : 'não encontrada'
-  });
-}
